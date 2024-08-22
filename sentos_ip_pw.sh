f25b8660b25cdba76723375e15d6287a1872cb97
huyang527 <huyang5277@gmail.com>
2024-08-23 02:33:54 +0800

    1

--- sentos_ip_pw.sh
+++ sentos_ip_pw.sh
@@ -5,9 +5,6 @@ echo "Kmsoft@km123456" | passwd --stdin root
 echo "请输入要设置的 IP 地址："
 read IP_ADDR
 
-echo "请输入子网掩码："
-read SUBNET_MASK
-
 echo "请输入网关地址："
 read GATEWAY
 
@@ -15,7 +12,7 @@ cat <<EOF > /etc/sysconfig/network-scripts/ifcfg-ens192
 DEVICE=ens192
 BOOTPROTO=static
 IPADDR=$IP_ADDR
-NETMASK=$SUBNET_MASK
+NETMASK=255.255.255.0
 GATEWAY=$GATEWAY
 ONBOOT=yes
 EOF
