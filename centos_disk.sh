5e9c6fa1fe71e22bd18d075191a13e9c13ee505b
huyang527 <huyang5277@gmail.com>
2024-08-23 18:04:56 +0800

    1

--- centos_disk.sh
+++ centos_disk.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
+fdisk -l
 
 echo "开始挂载新磁盘到/data目录,请输入新磁盘名称:(如/dev/sdb)"
-fdisk -l
 read DISK_ADD
 pvcreate $DISK_ADD
 
-echo "请输入卷组名称"
 vgdisplay -s
+echo "请输入卷组名称"
 read VG_NAME
-vgextend $VG_NAME /dev/$DISK_ADD
+vgextend $VG_NAME $DISK_ADD
 lvcreate -l 100%FREE -n data $VG_NAME
 mkfs.xfs /dev/$VG_NAME/data
 mkdir /data
 mount /dev/$VG_NAME/data /data
-echo "/dev/$VG_NAME/data /data xfs defaults 0 2" >> /etc/fstab
+echo "/dev/$VG_NAME/data /data xfs defaults 0 0" >> /etc/fstab
