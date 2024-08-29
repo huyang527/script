90ca32a9c10845a01b707bfc8186eb32159be521
huyang527 <huyang5277@gmail.com>
2024-08-29 19:45:19 +0800

    1

--- centos_disk.sh
+++ centos_disk.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 fdisk -l
 
-echo "开始挂载新磁盘到/data目录,请输入新磁盘名称:(如/dev/sdb)"
+echo "开始挂载新磁盘到/data目录,请输入新磁盘名称:(如/dev/sdb):"
 read DISK_ADD
+
 pvcreate $DISK_ADD
 
 vgdisplay -s
 echo "请输入卷组名称"
 read VG_NAME
+
 vgextend $VG_NAME $DISK_ADD
 lvcreate -l 100%FREE -n data $VG_NAME
 mkfs.xfs /dev/$VG_NAME/data
